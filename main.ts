8ee297e59288ed75ce0ac67235110a0633278973
10-biter <ep.student@hotmail.com>
2025-12-17 12:43:23 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@
      //%block="blink pixel at x $x y $y evry $ms ms with color $color"
      //%ms.shadow="timePicker"
      //%color.shadow="colorIndexPicker"
+     //%x.shadow="positionPicker"
+     //%y.shadow="positionPicker"
      export function blinkPixelAtX(x:number, y:number, ms:number, color:number): void {
 
      }
