4199fa65de0b981068d8d7913e0adb415a9bb582
10-biter <ep.student@hotmail.com>
2025-12-17 12:38:09 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+ namespace blinks {
+
+
+
+
+     //%block="blink pixel at x $x y $y evry $ms ms with color $color"
+     //%color.shadow="colorNumberPicker"
+     //%ms.shadow="timePicker"
+     export function blinkPixelAtX(x:number, y:number, ms:number, color:number): void {
+
+     }
+ }
